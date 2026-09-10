5667e269c49160d3df221b71239f9a94bff7b274
Shashank Manurey <167054687+shashankmanurey@users.noreply.github.com>
2026-09-10 09:40:56 +0530

    Day 3

---
+++ Day3_TC2.java
@@ -0,0 +1,32 @@
+package com.selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Day3_TC2 {
+
+    public static void main(String[] args) throws Exception {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+        driver.get("https://demoapps.qspiders.com/ui/datePick?sublist=0");
+        driver.findElement(By.xpath("//input")).click();
+        driver.findElement(By.xpath("//button[contains(@class,'react-datepicker__navigation--next')]")).click();
+        driver.findElement(By.xpath("//div[text()='10']")).click();
+        Thread.sleep(2000);
+        if(driver.getCurrentUrl().contains("date")) {
+       	 System.out.println("DateField-result page is displayed");
+        }else {
+       	 System.out.println("DateField-result page is not displayed");
+        }
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
